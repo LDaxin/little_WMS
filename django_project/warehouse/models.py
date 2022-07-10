558dff371ca09a29570f1b44f5687cb4ddb60069
LDaxin <ldaxinok@googlemail.com>
2022-07-10 14:33:59 +0200

    addet warehouse model

--- django_project/warehouse/models.py
+++ django_project/warehouse/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from hub import models as hub_models
 
 # Create your models here.
+
+    
+class Warehouse(models.Model):
+    location = models.ForeignKey(hub_models.Location)
+    active = models.BooleanField()
+    name = models.CharField(max_length=100)
+    prefix = models.CharField(max_length=3)
