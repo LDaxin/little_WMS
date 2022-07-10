674e95ff9d838267f46f0af11770777a99c0f3c5
LDaxin <ldaxinok@googlemail.com>
2022-07-10 14:33:43 +0200

    addet Location model

--- django_project/hub/models.py
+++ django_project/hub/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    traditional_land = models.CharField(max_length=50)
+    tradutional_country = models.CharField(max_length=50)
+    traditional_city = models.CharField(max_length=50)
+    traditional_zipcode = models.IntegerField()
+
+    modern_cordinates_longitudede = models.FloatField()
+    modern_cordinates_latitude = models.FloatField()
+
+    modern_what3words = models.CharField(max_length=50)
+    modern_what3words_lang = models.CharField(max_length=50)
