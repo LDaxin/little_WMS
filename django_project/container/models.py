c67fdec21fcface4590772977f300e9d35484631
LDaxin <ldaxinok@googlemail.com>
2022-07-11 22:16:46 +0200

    modified container Model

--- django_project/container/models.py
+++ django_project/container/models.py
@@ -1,3 +1,23 @@
+from sys import prefix
 from django.db import models
 
 # Create your models here.
+class Template(models.Model):
+    
+    outer_size_width = models.FloatField()
+    outer_size_height = models.FloatField()
+    outer_size_depth = models.FloatField()
+
+    inner_size_width = models.FloatField()
+    inner_size_height = models.FloatField()
+    inner_size_depth = models.FloatField()
+
+    empty_weight = models.FloatField()
+
+    prefix = models.CharField(max_length=6)
+
+
+class container(models.Model):
+    template = models.ForeignKey(Template)
+
+    prefix = models.CharField(max_length=10)
