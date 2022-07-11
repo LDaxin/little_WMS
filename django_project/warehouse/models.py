898b02043f175082014b3024dd1e123a96f823f7
LDaxin <ldaxinok@googlemail.com>
2022-07-11 22:17:03 +0200

    modified warehouse model

--- django_project/warehouse/models.py
+++ django_project/warehouse/models.py
@@ -1,11 +1,36 @@
+from sys import prefix
 from django.db import models
 from hub import models as hub_models
 
 # Create your models here.
 
-    
+
+
 class Warehouse(models.Model):
     location = models.ForeignKey(hub_models.Location)
     active = models.BooleanField()
     name = models.CharField(max_length=100)
-    prefix = models.CharField(max_length=3)
+
+    prefix = models.CharField(max_length=5)
+
+
+class Storage(models.Model):
+
+    compartments = models.IntegerField()
+
+    rows = models.IntegerField()
+    columns = models.IntegerField()
+    
+    prefix = models.CharField(max_length=5)
+
+class Compartment():
+    storage = models.ForeignKey(Storage)
+
+    size_width = models.FloatField()
+    size_height = models.FloatField()
+    size_depth = models.FloatField()
+
+    row = models.IntegerField()
+    colum = models.IntegerField()
+
+    prefix = models.CharField(max_length=6)
