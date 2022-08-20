c6205db087bc165285b97a44c23383fd70875b96
Adrian <ldaxinok@googlemail.com>
2022-08-20 11:54:51 +0200

    added apps

--- django_project/little_wms/settings.py
+++ django_project/little_wms/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hub.apps.HubConfig',
+    'container.apps.ContainerConfig',
+    'part.apps.PartConfig',
+    'warehouse.apps.WarehouseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
