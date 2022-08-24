3d2a8ee0fc6b852b275b600acf963540565fc339
Adrian <ldaxinok@googlemail.com>
2022-08-24 20:06:29 +0200

    modified the part model

--- django_project/part/admin.py
+++ django_project/part/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from part.models import *
 
 # Register your models here.
+
+admin.site.register(Part)
--- django_project/part/models.py
+++ django_project/part/models.py
@@ -1,18 +1,46 @@
+import turtle
 from django.db import models
+
+from django_project.warehouse.models import Warehouse
+from warehouse.models import Compartment
+from container.models import Container
 
 # Create your models here.
 
 class Tag(models.Model):
     name = models.CharField(max_length=20)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
 
 
 class Template(models.Model):
+    quantity_type_choices = (
+        ("PC", "pieces"),
+        ("g", "weight"),
+        ("m", "length"),
+    )
+
+    name = models.CharField(200)
+
     weight = models.FloatField()
-    tag = models.ForeignKey(Tag, on_delete=models.RESTRICT)
+
+    quantity_type = models.CharField(max_length=4, choices=quantity_type_choices)
+
+    length = models.FloatField()
+    width = models.FloatField()
+    depth = models.FloatField()
+
+    tag = models.ManyToManyField(Tag)
 
     
 class Part(models.Model):
     template = models.ForeignKey(Template, on_delete=models.RESTRICT)
-    tag = models.ManyToManyField(Tag)
+    
+    container = models.ForeignKey(Container, on_delete=models.SET_NULL, blank=True, null=True)
+    compartment = models.ForeignKey(Compartment, on_delete=models.SET_NULL, blank=True, null=True)
+    
+    
+
+    quantity = models.FloatField()
+
+    tag = models.ManyToManyField(Tag, blank=True, null=True)
 
