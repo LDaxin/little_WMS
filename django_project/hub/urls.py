ca75bd2f33301dcd88c596abfc9321d94312b118
LDaxin <ldaxinok@googlemail.com>
2022-08-27 11:52:10 +0200

    worked on the views Location and manage

--- django_project/hub/urls.py
+++ django_project/hub/urls.py
@@ -5,6 +5,8 @@ from part import views as partView
 
 urlpatterns = [
     path("",views.hub, name="hub"),
+    path("manage/",views.manage, name="manage"),
+    path("manage/locations", views.locations, name="locations"),
     
     # path("part/", partView.part, name="part"),
 ]
--- django_project/hub/views.py
+++ django_project/hub/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from hub.models import Location
 
 # Create your views here.
 
 def hub(request):
     return render(request, "hub/hub.html")
 
+def manage(request):
+    return render(request, "hub/manage.html")
+
+def locations(request):
+
+    return render(request, "hub/locations.html", context={"location":Location.objects.all()})
