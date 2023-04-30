c9a399c4735c9b9eb9bd327e166a18c4c322c7fa
LDaxin <46381046+LDaxin@users.noreply.github.com>
2023-04-30 09:00:49 +0200

    fiexed articles didnt show

--- django_project/hub/views.py
+++ django_project/hub/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template.loader import render_to_string
 from .models import *
-import article.models as pa
+import article.models as ar
 #from storage.models import *
 import storage.models as st
 from .forms import *
@@ -16,7 +16,7 @@ from django.db.models import Q
 
 @login_required(login_url='/accounts/login/')
 def hub(request):
-    return render(request, "hub/hub.html", context={"symbol":"Logo", "fields":['Check_in', 'Check_out', 'Article', 'Storage'], "paType":pa.ArticleType.objects.all(), "stType":st.StorageType.objects.all()})
+    return render(request, "hub/hub.html", context={"symbol":"Logo", "fields":['Check_in', 'Check_out', 'Article', 'Storage'], "paType":ar.ArticleType.objects.all(), "stType":st.StorageType.objects.all()})
 
 
 
@@ -66,10 +66,11 @@ def results(request):
     if request.method == "GET":
         if request.GET['type']=="article":
             if request.GET['search'] == "NONE":
-                r = Article.objects.filter(template__pType__tName__exact=request.GET["ptype"])
+                r = ar.Article.objects.filter(template__pType__tName__exact=request.GET["ptype"])
             else:
                 #r = Article.objects.filter(template__name__contains=request.GET['search'], template__pType__tName__exact=request.GET["ptype"])
-                r = Article.objects.filter(Q(template__name__contains=request.GET['search']) | Q(code__contains=request.GET['search']), template__pType__tName__exact=request.GET["ptype"])
+
+                r = ar.Article.objects.filter(Q(template__name__contains=request.GET['search']) | Q(code__contains=request.GET['search']), template__pType__tName__exact=request.GET["ptype"])
             return render(request, "hub/modules/results.html", context={"results":r, "type":"article"})
 
         elif request.GET['type']=="storage":
