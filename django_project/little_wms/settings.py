569ff78040ca28c99e815dd07f58e655e2791b28
LDaxin <ldaxinok@googlemail.com>
2023-12-16 13:26:43 +0100

    fixed problems with docker deployment

--- django_project/little_wms/settings.py
+++ django_project/little_wms/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = bool(os.environ.get("DEBUG", default=False))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 LOGIN_REDIRECT_URL = "/"
