a5e5824a1ca5e0925deaf52aba49f4f6b5661242
Adrian <ldaxinok@googlemail.com>
2025-02-13 10:40:44 +0100

    fixed scanner not inputing in field in modal

--- django_project/codeSystem/admin.py
+++ django_project/codeSystem/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from codeSystem.models import *
 
+class UuidCodeAdmin(admin.ModelAdmin):
+    readonly_fields = ('uuidCode',)
+
 # Register your models here.
 admin.site.register(UuidCode)
